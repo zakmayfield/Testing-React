5e023d7ad2934273150a91145428d3a0a7ae94ee
Zachary Mayfield <zakmayfield@gmail.com>
2020-01-27 23:13:10 -0500

    actually think i've made MVP on this commit, i came across another way to do what i needed to do, and now my tests are passing

--- star-wars-app/src/components/StarWarsCharacters.test.js
+++ star-wars-app/src/components/StarWarsCharacters.test.js
@@ -5,30 +5,59 @@ import { getData as mockGetData } from '../api'
 
 jest.mock("../api")
 
-mockGetData.mockResolvedValue({
-  next: "Next",
-  previous: "Previous",
-  results: [
-    {
-      name: 'Test Name',
-      url: 'Test Url'
-    }
-  ]
-})
+// mockGetData.mockResolvedValue({
+//   count: "80",
+//   next: "Next",
+//   previous: "Previous",
+//   results: [
+//     {
+//       name: 'Test Name',
+//       url: 'Test Url'
+//     }
+//   ]
+// })
 
 test('renders component', async () => {
+  const testData = {
+    count: "80",
+    next: "Next",
+    previous: "Previous",
+    results: [
+      {
+        name: 'Test Name',
+        url: 'Test Url'
+      }
+    ]
+  }
+  mockGetData.mockResolvedValueOnce(testData)
+
   const component = render(<StarWarsCharacters />);
 })
 
-test('renders buttons', async () => {
+test('api test', async () => {
+  const testData = {
+    count: "80",
+    next: "Next",
+    previous: "Previous",
+    results: [
+      {
+        name: 'Test Name',
+        url: 'Test Url'
+      }
+    ]
+  }
+  mockGetData.mockResolvedValueOnce(testData)
+
   const component = render(<StarWarsCharacters />);
 
-  const prevBtn = component.getByTestId('prevBtn')
   const nextBtn = component.getByTestId('nextBtn');
 
   fireEvent.click(nextBtn)
 
   expect(mockGetData).toHaveBeenCalledTimes(2)
 
-  await wait (() => expect(component.getByText(/Test Name/i)))
+  await wait (() => {
+    expect(component.getByText(/Test Name/i)).toBeTruthy();
+    expect(mockGetData).toHaveBeenCalledTimes(2)
+  })
 })
